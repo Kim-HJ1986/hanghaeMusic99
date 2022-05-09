18b2e01d01995e9181e79e71a31697cc82b07f6b
Jun Kim <rlafbf222@naver.com>
2022-05-09 20:49:00 +0900

    현재 간단한 로그인, 크롤링, 디테일페이지 구현 중.

---
+++ insertMusicToDB.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.6pe7g.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.hanghaemusic99
+
+#마치 사람이 요청을 날린 것 처럼 보여주기 위해 사용, 브라우저에서 요청한 것처럼됨. (실제론 코드에서 날림)
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+data = requests.get('https://www.melon.com/chart/',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+musics_50 = soup.select('#lst50')
+musics_100 = soup.select('#lst100')
+all_musics = musics_50 + musics_100
+
+#print(musics)
+id = 1
+for music in all_musics:
+    rank = music.select_one('td:nth-child(2) > div > span.rank ').text
+    title = music.select_one('td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a').text
+    singer = music.select_one('td:nth-child(6) > div > div > div.ellipsis.rank02 > a').text
+    image_url = music.select_one('td:nth-child(4) > div > a > img')['src']
+
+    doc = {
+        'id': id,
+        'rank': rank,
+        'title': title,
+        'singer': singer,
+        'image_url': image_url
+    }
+
+    db.music_list.insert_one(doc)
+    id += 1
+
